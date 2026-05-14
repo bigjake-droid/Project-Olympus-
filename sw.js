3bb44f5f00e506b8ac417d9f6deb4a60ffe41b01
bigjake-droid <manningjake2018@gmail.com>
2026-05-14 06:44:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,15 @@
-const CACHE_NAME = 'vindex-tactical-cache-v2';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/dashboard.html',
-  '/evidence.html',
-  '/guidebook.html',
-  '/style.css',
-  '/script.js'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => caches.delete(key)));
     }).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
